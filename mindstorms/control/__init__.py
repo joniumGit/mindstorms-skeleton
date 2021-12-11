0921fa7e439532e26b7ccaddaf86abb90437feb4
joniumGit <52005121+joniumGit@users.noreply.github.com>
2021-12-12 00:02:03 +0200

    fix wrong type hint

--- mindstorms/control/__init__.py
+++ mindstorms/control/__init__.py
@@ -1,14 +1,13 @@
 from typing import Callable, TypeVar
 
 _T = TypeVar('_T')
-_V = TypeVar('_V')
 
 
 def wait_for_seconds(seconds: int, /):
     pass
 
 
-def wait_until(get_value_function: Callable[[], _T], operator_function: Callable[[_T], _V], target_value: _V, /):
+def wait_until(get_value_function: Callable[[], _T], operator_function: Callable[[_T, _T], bool], target_value: _T, /):
     pass
 
 
